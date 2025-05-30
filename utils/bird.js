ba577888615b589cfddca6aa90165735d9556769
ibouvousaime <ibouvousaime@gmail.com>
2025-05-30 14:45:39 +0200

    fixes

--- utils/bird.js
+++ utils/bird.js
@@ -1,55 +1,62 @@
 const { Rettiwt } = require("rettiwt-api");
-const rettiwt = new Rettiwt();
+const dotenv = require("dotenv");
+dotenv.config();
+const rettiwt = new Rettiwt({ apiKey: process.env.TWITTER_API_KEY });
 const instagramDl = require("@sasmeee/igdl");
 
 function extractTweetId(url) {
-	const regex = /status\/(\d+)/;
-	const match = url.match(regex);
+  const regex = /status\/(\d+)/;
+  const match = url.match(regex);
 
-	if (match) {
-		const tweetId = match[1];
-		return tweetId;
-	} else {
-		return null;
-	}
+  if (match) {
+    const tweetId = match[1];
+    return tweetId;
+  } else {
+    return null;
+  }
 }
 
 function extractTweet(url) {
-	return new Promise((resolve, reject) => {
-		const tweetId = extractTweetId(url);
-		rettiwt.tweet
-			.details(tweetId)
-			.then((res) => {
-				resolve(res);
-			})
-			.catch((err) => {
-				console.error(err);
-				reject();
-			});
-	});
+  return new Promise((resolve, reject) => {
+    const tweetId = extractTweetId(url);
+    console.log(tweetId);
+    rettiwt.user.details().then((res) => {
+      console.log(res); // Returns details of the user associated with API_KEY_1
+    });
+    rettiwt.tweet
+      .details(tweetId, "id")
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        console.error(err);
+        reject();
+      });
+  });
 }
 
 function extractInstaLinks(text) {
-	const reelRegex = /(?:https?:\/\/)?(?:www\.)?instagram\.com\/reel\/[a-zA-Z0-9_-]+\/?/g;
-	const reelLinks = text.match(reelRegex);
-	return reelLinks || [];
+  const reelRegex =
+    /(?:https?:\/\/)?(?:www\.)?instagram\.com\/reel\/[a-zA-Z0-9_-]+\/?/g;
+  const reelLinks = text.match(reelRegex);
+  return reelLinks || [];
 }
 
 function getInstagramVideoLink(text) {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const links = extractInstaLinks(text);
-			console.log(links);
-			let output = [];
-			for (let link of links) {
-				console.log(link);
-				output.push(await instagramDl(link));
-			}
-			resolve(output);
-		} catch (err) {
-			reject(err);
-		}
-	});
+  return new Promise(async (resolve, reject) => {
+    try {
+      const links = extractInstaLinks(text);
+      console.log(links);
+      let output = [];
+      for (let link of links) {
+        console.log(link);
+        output.push(await instagramDl(link));
+      }
+      resolve(output);
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
 
 module.exports = { extractTweet, extractTweetId, getInstagramVideoLink };
