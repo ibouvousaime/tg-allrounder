dd1892a6966c409bd2de59488a5d2678917d8f82
ibouvousaime <ibouvousaime@gmail.com>
2026-04-16 22:43:53 +0200

    clean

--- utils/downloader.js
+++ utils/downloader.js
@@ -11,279 +11,270 @@ const path = require("path");
 const crypto = require("crypto");
 
 async function loadMusicModule() {
-  const musicModule = await import("ytmusic-api");
+	const musicModule = await import("ytmusic-api");
 
-  const YTMusic = musicModule.default;
-  return YTMusic;
+	const YTMusic = musicModule.default;
+	return YTMusic;
 }
 
 function spawnPromise(command, args) {
-  return new Promise((resolve, reject) => {
-    const child = spawn(command, args, {
-      shell: true,
-    });
+	return new Promise((resolve, reject) => {
+		const child = spawn(command, args, {
+			shell: true,
+		});
 
-    child.stdout.on("data", (data) => {
-      process.stdout.write(data);
-    });
+		child.stdout.on("data", (data) => {
+			process.stdout.write(data);
+		});
 
-    child.stderr.on("data", (data) => {
-      process.stderr.write(data);
-    });
+		child.stderr.on("data", (data) => {
+			process.stderr.write(data);
+		});
 
-    child.on("error", reject);
+		child.on("error", reject);
 
-    child.on("close", (code) => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(new Error(`yt-dlp exited with code ${code}`));
-      }
-    });
-  });
+		child.on("close", (code) => {
+			if (code === 0) {
+				resolve();
+			} else {
+				reject(new Error(`yt-dlp exited with code ${code}`));
+			}
+		});
+	});
 }
 
 async function extractAndEchoSocialLink(text, callback) {
-  let audioMode = false;
-  let filename = "video";
-  const { spotifyConvertedLink, name } = await getSpotifyMusicLink(text);
-  if (spotifyConvertedLink) {
-    text = spotifyConvertedLink;
-    audioMode = true;
-    filename = name;
-  }
-  const socialLinkRegex =
-    /(https?:\/\/(www\.)?((tiktok\.com|vm\.tiktok\.com|music\.youtube\.com|soundcloud\.com|on\.soundcloud\.com)\/|youtube\.com\/shorts\/)[^\s]+)/;
+	let audioMode = false;
+	let filename = "video";
+	const { spotifyConvertedLink, name } = await getSpotifyMusicLink(text);
+	if (spotifyConvertedLink) {
+		text = spotifyConvertedLink;
+		audioMode = true;
+		filename = name;
+	}
+	const socialLinkRegex =
+		/(https?:\/\/(www\.)?((tiktok\.com|vm\.tiktok\.com|music\.youtube\.com|soundcloud\.com|on\.soundcloud\.com)\/|youtube\.com\/shorts\/)[^\s]+)/;
 
-  const match = text.match(socialLinkRegex);
-  if (!match || !match[0]) {
-    return;
-  }
+	const match = text.match(socialLinkRegex);
+	if (!match || !match[0]) {
+		return;
+	}
 
-  const link = match[0];
-  if (link.includes("soundcloud")) audioMode = true;
-  const destinationFolder = path.join(os.tmpdir(), makeid(20));
-  fs.mkdirSync(destinationFolder);
-  const ytDlpCommand = [
-    `${os.homedir()}/.local/bin/yt-dlp`,
-    `--embed-metadata`,
-    `"${link}"`,
-    `-P "${destinationFolder}"`,
-    audioMode
-      ? '-x --audio-format mp3 -f "bestaudio/best"'
-      : '-f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"',
-    `--cookies ${os.homedir()}/cookies.txt`,
-  ].join(" ");
-  const galleryDlCommand = `${os.homedir()}/.local/bin/gallery-dl "${link}" --dest "${destinationFolder}" --cookies ${os.homedir()}/cookies.txt`;
-  try {
-    try {
-      await execPromise(ytDlpCommand);
-    } catch (ytDlpError) {
-      console.error(`yt-dlp failed, trying gallery-dl...`);
-      await execPromise(galleryDlCommand);
-      usedGalleryDL = true;
-    }
+	const link = match[0];
+	if (link.includes("soundcloud")) audioMode = true;
+	const destinationFolder = path.join(os.tmpdir(), makeid(20));
+	fs.mkdirSync(destinationFolder);
+	const ytDlpCommand = [
+		`${os.homedir()}/.local/bin/yt-dlp`,
+		`--embed-metadata`,
+		`"${link}"`,
+		`-P "${destinationFolder}"`,
+		audioMode ? '-x --audio-format mp3 -f "bestaudio/best"' : '-f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"',
+		`--cookies ${os.homedir()}/cookies.txt`,
+	].join(" ");
+	const galleryDlCommand = `${os.homedir()}/.local/bin/gallery-dl "${link}" --dest "${destinationFolder}" --cookies ${os.homedir()}/cookies.txt`;
+	try {
+		try {
+			await execPromise(ytDlpCommand);
+		} catch (ytDlpError) {
+			console.error(`yt-dlp failed, trying gallery-dl...`);
+			await execPromise(galleryDlCommand);
+			usedGalleryDL = true;
+		}
 
-    //await flattenDirectory(destinationFolder);
-    const files = getAllFilesSync(destinationFolder);
-    if (files) {
-      await callback(files);
-    } else {
-      console.error("Download succeeded, but no file was found.");
-    }
-  } catch (error) {
-    console.error(`All download attempts failed: ${error}`);
-  } finally {
-    setTimeout(() => {
-      if (fs.existsSync(destinationFolder)) {
-        fs.rmSync(destinationFolder, { recursive: true, force: true });
-      }
-    }, 5000);
-  }
+		//await flattenDirectory(destinationFolder);
+		const files = getAllFilesSync(destinationFolder);
+		if (files) {
+			await callback(files);
+		} else {
+			console.error("Download succeeded, but no file was found.");
+		}
+	} catch (error) {
+		console.error(`All download attempts failed: ${error}`);
+	} finally {
+		setTimeout(() => {
+			if (fs.existsSync(destinationFolder)) {
+				fs.rmSync(destinationFolder, { recursive: true, force: true });
+			}
+		}, 5000);
+	}
 }
 
 async function downloadVideoFromUrl(text, forceAudio, callback) {
-  const urlRegex = /(https?:\/\/[^\s]+)/;
-  const match = text.match(urlRegex);
+	const urlRegex = /(https?:\/\/[^\s]+)/;
+	const match = text.match(urlRegex);
 
-  if (!match || !match[0]) {
-    console.error("No URL found in the provided text.");
-    return;
-  }
+	if (!match || !match[0]) {
+		console.error("No URL found in the provided text.");
+		return;
+	}
 
-  const link = match[0];
-  const destinationFolder = path.join(os.tmpdir(), makeid(20));
-  fs.mkdirSync(destinationFolder);
+	const link = match[0];
+	const destinationFolder = path.join(os.tmpdir(), makeid(20));
+	fs.mkdirSync(destinationFolder);
 
-  let isAudioOnly = false;
-  if (forceAudio) {
-    isAudioOnly = true;
-  }
-  const ytdlpPath = `${os.homedir()}/.local/bin/yt-dlp`;
-  const ytDlpCommandArgs = [
-    "--embed-metadata",
-    `"${link}"`,
-    `-P "${destinationFolder}"`,
+	let isAudioOnly = false;
+	if (forceAudio) {
+		isAudioOnly = true;
+	}
+	const ytdlpPath = `${os.homedir()}/.local/bin/yt-dlp`;
+	const ytDlpCommandArgs = [
+		"--embed-metadata",
+		`"${link}"`,
+		`-P "${destinationFolder}"`,
 
-    isAudioOnly
-      ? '-x --audio-format mp3 -f "bestaudio/best"'
-      : '-f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"',
-    " --trim-filenames 10",
-    "--merge-output-format mp4",
-    '--postprocessor-args "ffmpeg:-movflags +faststart"',
-    `--cookies ${os.homedir()}/cookies.txt`,
-  ];
-  const ytdlpUpdateCommand = `${ytdlpPath} -U`;
-  try {
-    await execPromise(ytdlpUpdateCommand);
-  } catch (updateError) {
-    console.error("yt-dlp update failed:", updateError);
-  }
-  try {
-    await spawnPromise(ytdlpPath, ytDlpCommandArgs);
+		isAudioOnly ? '-x --audio-format mp3 -f "bestaudio/best"' : '-f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"',
+		" --trim-filenames 10",
+		"--merge-output-format mp4",
+		'--postprocessor-args "ffmpeg:-movflags +faststart"',
+		`--cookies ${os.homedir()}/cookies.txt`,
+	];
+	const ytdlpUpdateCommand = `${ytdlpPath} -U`;
+	try {
+		await execPromise(ytdlpUpdateCommand);
+	} catch (updateError) {
+		console.error("yt-dlp update failed:", updateError);
+	}
+	try {
+		await spawnPromise(ytdlpPath, ytDlpCommandArgs);
 
-    const files = getAllFilesSync(destinationFolder);
-    if (files && files.length > 0) {
-      await callback(files);
-    } else {
-      console.error("no files found after download command");
-    }
-  } catch (error) {
-    console.error(`download error: "${link}":`, error);
-  } finally {
-    setTimeout(() => {
-      if (fs.existsSync(destinationFolder)) {
-        fs.rmSync(destinationFolder, { recursive: true, force: true });
-      }
-    }, 10000);
-  }
+		const files = getAllFilesSync(destinationFolder);
+		if (files && files.length > 0) {
+			await callback(files);
+		} else {
+			console.error("no files found after download command");
+		}
+	} catch (error) {
+		console.error(`download error: "${link}":`, error);
+	} finally {
+		setTimeout(() => {
+			if (fs.existsSync(destinationFolder)) {
+				fs.rmSync(destinationFolder, { recursive: true, force: true });
+			}
+		}, 10000);
+	}
 }
 
 const getAllFilesSync = (dirPath, arrayOfFiles = []) => {
-  const files = fs.readdirSync(dirPath);
-  files.forEach((file) => {
-    const fullPath = path.join(dirPath, file);
+	const files = fs.readdirSync(dirPath);
+	files.forEach((file) => {
+		const fullPath = path.join(dirPath, file);
 
-    if (fs.statSync(fullPath).isDirectory()) {
-      getAllFilesSync(fullPath, arrayOfFiles);
-    } else {
-      arrayOfFiles.push(fullPath);
-    }
-  });
+		if (fs.statSync(fullPath).isDirectory()) {
+			getAllFilesSync(fullPath, arrayOfFiles);
+		} else {
+			arrayOfFiles.push(fullPath);
+		}
+	});
 
-  return arrayOfFiles;
+	return arrayOfFiles;
 };
 
 function findSpotifyTrackLink(text) {
-  const spotifyRegex =
-    /(?:https?:\/\/open\.spotify\.com\/|spotify:)(track|album)[:\/]([a-zA-Z0-9]+)/;
+	const spotifyRegex = /(?:https?:\/\/open\.spotify\.com\/|spotify:)(track|album)[:\/]([a-zA-Z0-9]+)/;
 
-  const matches = text.match(spotifyRegex);
+	const matches = text.match(spotifyRegex);
 
-  if (matches) {
-    const fullLink = matches[0];
-    const type = matches[1];
-    const id = matches[2];
+	if (matches) {
+		const fullLink = matches[0];
+		const type = matches[1];
+		const id = matches[2];
 
-    return { fullLink, type, id };
-  } else {
-    return { fullLink: null, type: null, id: null };
-  }
+		return { fullLink, type, id };
+	} else {
+		return { fullLink: null, type: null, id: null };
+	}
 }
 
 async function getSpotifyMusicLink(link) {
-  const { fullLink, type } = findSpotifyTrackLink(link);
-  if (fullLink) {
-    const YTMusic = await loadMusicModule();
-    const ytmusic = new YTMusic();
-    await ytmusic.initialize({
-      cookies: fs
-        .readFileSync(path.join(os.homedir(), "cookies.txt"))
-        .toString(),
-    });
+	try {
+		const { fullLink, type } = findSpotifyTrackLink(link);
+		if (fullLink) {
+			const YTMusic = await loadMusicModule();
+			const ytmusic = new YTMusic();
+			await ytmusic.initialize({
+				cookies: fs.readFileSync(path.join(os.homedir(), "cookies.txt")).toString(),
+			});
 
-    if (type == "track") {
-      const songData = await SpottyDL.getTrack(fullLink);
-      const fullSongName = `${songData.artist} - ${songData.title}`;
-      const result = await ytmusic.searchSongs(fullSongName);
-      return {
-        spotifyConvertedLink: `https://music.youtube.com/watch?v=${result[0].videoId}`,
-        name: fullSongName,
-      };
-    } else if (type == "album") {
-      const albumData = await SpottyDL.getAlbum(fullLink);
-      const fullAlbumName = `${albumData.artist} - ${albumData.name}}`;
-      const youtubeAlbums = await ytmusic.searchAlbums(fullAlbumName);
-      return {
-        spotifyConvertedLink: `https://music.youtube.com/playlist?list=${youtubeAlbums[0].playlistId}`,
-        name: fullAlbumName,
-      };
-    }
-  } else return { spotifyConvertedLink: null, name: null };
+			if (type == "track") {
+				const songData = await SpottyDL.getTrack(fullLink);
+				console.log("song data", songData);
+				const fullSongName = `${songData.artist} - ${songData.title}`;
+				const result = await ytmusic.searchSongs(fullSongName);
+				return {
+					spotifyConvertedLink: `https://music.youtube.com/watch?v=${result[0].videoId}`,
+					name: fullSongName,
+				};
+			} else if (type == "album") {
+				const albumData = await SpottyDL.getAlbum(fullLink);
+				const fullAlbumName = `${albumData.artist} - ${albumData.name}}`;
+				const youtubeAlbums = await ytmusic.searchAlbums(fullAlbumName);
+				return {
+					spotifyConvertedLink: `https://music.youtube.com/playlist?list=${youtubeAlbums[0].playlistId}`,
+					name: fullAlbumName,
+				};
+			}
+		} else return { spotifyConvertedLink: null, name: null };
+	} catch (err) {
+		console.error(err);
+		return { spotifyConvertedLink: null, name: null };
+	}
 }
 
 async function getAlbumFromSong(songName) {
-  const YTMusic = await loadMusicModule();
-  const ytmusic = new YTMusic();
-  await ytmusic.initialize();
-  const youtubeAlbums = await ytmusic.searchAlbums(songName);
-  return {
-    albumLink: `https://music.youtube.com/playlist?list=${youtubeAlbums[0].playlistId}`,
-    name: `${youtubeAlbums[0].artist.name} - ${youtubeAlbums[0].name}`,
-  };
+	const YTMusic = await loadMusicModule();
+	const ytmusic = new YTMusic();
+	await ytmusic.initialize();
+	const youtubeAlbums = await ytmusic.searchAlbums(songName);
+	return {
+		albumLink: `https://music.youtube.com/playlist?list=${youtubeAlbums[0].playlistId}`,
+		name: `${youtubeAlbums[0].artist.name} - ${youtubeAlbums[0].name}`,
+	};
 }
 
 async function downloadImageAsBuffer(url) {
-  try {
-    const response = await axios.get(url, {
-      responseType: "arraybuffer",
-    });
+	try {
+		const response = await axios.get(url, {
+			responseType: "arraybuffer",
+		});
 
-    const imageBuffer = Buffer.from(response.data, "binary");
+		const imageBuffer = Buffer.from(response.data, "binary");
 
-    return imageBuffer;
-  } catch (error) {
-    throw new Error(`code: ${error.response ? error.response.status : "N/A"}`);
-  }
+		return imageBuffer;
+	} catch (error) {
+		throw new Error(`code: ${error.response ? error.response.status : "N/A"}`);
+	}
 }
 
 async function extractAudioFromVideo(inputVideoPath, callback) {
-  if (!fs.existsSync(inputVideoPath)) {
-    throw new Error("Input video does not exist");
-  }
+	if (!fs.existsSync(inputVideoPath)) {
+		throw new Error("Input video does not exist");
+	}
 
-  const tmpId = crypto.randomUUID();
-  const tempDir = path.join(os.tmpdir(), `audioextract-${tmpId}`);
-  const outputAudioPath = path.join(tempDir, "audio.mp3");
+	const tmpId = crypto.randomUUID();
+	const tempDir = path.join(os.tmpdir(), `audioextract-${tmpId}`);
+	const outputAudioPath = path.join(tempDir, "audio.mp3");
 
-  fs.mkdirSync(tempDir, { recursive: true });
+	fs.mkdirSync(tempDir, { recursive: true });
 
-  const ffmpegCmd = [
-    "ffmpeg",
-    "-y",
-    `-i "${inputVideoPath}"`,
-    "-vn",
-    "-acodec libmp3lame",
-    "-q:a 2",
-    `"${outputAudioPath}"`,
-  ].join(" ");
+	const ffmpegCmd = ["ffmpeg", "-y", `-i "${inputVideoPath}"`, "-vn", "-acodec libmp3lame", "-q:a 2", `"${outputAudioPath}"`].join(" ");
 
-  try {
-    await execPromise(ffmpegCmd);
-    await callback({
-      tempDir,
-      outputAudioPath,
-    });
-  } finally {
-    fs.rmSync(tempDir, { recursive: true, force: true });
-  }
+	try {
+		await execPromise(ffmpegCmd);
+		await callback({
+			tempDir,
+			outputAudioPath,
+		});
+	} finally {
+		fs.rmSync(tempDir, { recursive: true, force: true });
+	}
 }
 
 module.exports = {
-  extractAndEchoSocialLink,
-  getSpotifyMusicLink,
-  downloadImageAsBuffer,
-  getAlbumFromSong,
-  downloadVideoFromUrl,
-  extractAudioFromVideo,
+	extractAndEchoSocialLink,
+	getSpotifyMusicLink,
+	downloadImageAsBuffer,
+	getAlbumFromSong,
+	downloadVideoFromUrl,
+	extractAudioFromVideo,
 };
